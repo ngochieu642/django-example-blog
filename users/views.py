d96fee4042118fd7b3fc0787710ba81954e4bc11
ngochieu642 <ngochieu10041997@gmail.com>
2020-01-09 20:51:52 +0700

    Update login logout middleware

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -18,9 +18,13 @@ def register(request):
             messages.success(request, f"Account created for {username}!")
 
             # Return front end
-            return redirect("blog-home")
+            return redirect("login")
 
     else:
         # GET request
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
