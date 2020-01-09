b3fa5b910761ffa3ba74c1103d30fc3d541b4d30
ngochieu642 <ngochieu10041997@gmail.com>
2020-01-09 18:30:32 +0700

    database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+# Post model
+class Post(models.Model):
+    # id = models.CharField(primary_key=True, max_length=80)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # tags = ArrayField(models.CharField(max_length=80, blank=True), size=3)
+
