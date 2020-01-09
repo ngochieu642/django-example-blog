73bc1557777d4dfd9c0d8eaebf748d7a6eda0d66
ngochieu642 <ngochieu10041997@gmail.com>
2020-01-09 07:04:29 +0700

    Add templates

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "hieu thai",
+        "title": "blog post 1",
+        "content": "hello world",
+        "date_posted": "Dec 2019",
+    },
+    {
+        "author": "Khoa Dam",
+        "title": "blog post 2",
+        "content": "hello react",
+        "date_posted": "Jan 2020",
+    },
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts':posts
+    }
+    return render(request, "blog/home.html", context=context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html")
 
