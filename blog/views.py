d267aa34bde21f035f5627706a7c6a3cd8dadc08
ngochieu642 <ngochieu10041997@gmail.com>
2020-01-09 18:54:47 +0700

    remove dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        "author": "hieu thai",
-        "title": "blog post 1",
-        "content": "hello world",
-        "date_posted": "Dec 2019",
-    },
-    {
-        "author": "Khoa Dam",
-        "title": "blog post 2",
-        "content": "hello react",
-        "date_posted": "Jan 2020",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {
-        'posts':posts
-    }
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context=context)
 
 
 def about(request):
-    return render(request, "blog/about.html", context={'title':'About'})
+    return render(request, "blog/about.html", context={"title": "About"})
 
